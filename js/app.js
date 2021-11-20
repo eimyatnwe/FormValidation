4f9acbdb8d2d4f3a8b2f4d1c394362177cc23047
EiMyatNwe <73009458+eimyatnwe@users.noreply.github.com>
2021-11-20 09:43:56 +0630

    Update app.js

--- js/app.js
+++ js/app.js
@@ -6,51 +6,6 @@ const email=document.getElementById("email");
 const password=document.getElementById("password");
 const cfmpassword=document.getElementById("cfmpassword");
 
-
-// Event Listener(Method I)
-
-// form.addEventListener("submit",function(e){
-
-// 	e.preventDefault();
-// 	// console.log("hay");
-
-// 	if(username.value === ''){
-
-// 		showerror(username,"Username is required");
-		
-// 	}else{
-
-// 		showsuccess(username);
-// 	}
-
-// 	if(email.value === ''){
-
-// 		showerror(email,"Email is required");
-// 	}else if(!validateEmail(email.value)){
-
-// 		showerror(email,"Email is not valid!");
-
-// 	}else{
-
-// 		showsuccess(email);
-// 	}
-
-// 	if(password.value === ''){
-// 		showerror(password,"Password is required");
-// 	}else{
-// 		showsuccess(password);
-// 	}
-
-// 	if(cfmpassword.value === ''){
-// 		showerror(cfmpassword,"Confirm Password is required");
-// 	}else if(password.value != cfmpassword.value){
-// 		showerror(cfmpassword,"Password do not match");
-// 	}else{
-// 		showsuccess(cfmpassword);
-// 	}
-// });
-
-
 function showerror(input,message){
 
 	const formcontrol = input.parentElement;
@@ -68,24 +23,14 @@ function showsuccess(input){
 	formcontrol.classList.add("success");
 }
 
-
-// for checking email format
-
-// function validateEmail(email) {
-//     const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-//     return re.test(String(email).toLowerCase());
-// }
-
-               // Start Of Method II
+  // Start Of Method II
 
 // Check Required Field
 function checkrequired(inputarrs){
 	// console.log(inputarrs);
 
 	inputarrs.forEach(function(inputarr){
-		// console.log(inputarr);
-		// console.log(inputarr.value);
-		// console.log(inputarr.id);
+		
 		if(inputarr.value === ''){
 			showerror(inputarr,`${getfieldname(inputarr)} is required`);
 		}else{
